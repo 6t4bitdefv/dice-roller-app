8af5535e015f635d06a90014393ebd8797e8bf86
6t4bitdefv <strictly4github@gmail.com>
2023-01-03 17:13:45 +0100

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-let x;
-let y;
-let z;
-
-document.getElementById("rollButton").onclick = function () {
-  x = Math.floor(Math.random() * 6) + 1;
-  y = Math.floor(Math.random() * 6) + 1;
-  z = Math.floor(Math.random() * 6) + 1;
-
-  document.getElementById("xLabel").textContent = `Dice 1: ${x}`;
-  document.getElementById("yLabel").textContent = `Dice 2: ${y}`;
-  document.getElementById("zLabel").textContent = `Dice 3: ${z}`;
+document.getElementById("myButton").onclick = function () {
+  const myCheckBox = document.getElementById("myCheckBox");
+  if (myCheckBox.checked) {
+    alert("You are subscribed.");
+  } else {
+    alert("You are NOT subscribed.");
+  }
 };
